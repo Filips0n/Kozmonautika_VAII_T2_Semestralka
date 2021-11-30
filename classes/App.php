6b1bd118b97e4a9ccfef2177e05b7115f86f4207
Filipson <filip.sudora@gmail.com>
2021-11-30 15:24:24 +0100

    Added Id validation check

--- classes/App.php
+++ classes/App.php
@@ -39,10 +39,11 @@ class App
                 $human_rated = $this->test_input($_POST["rocket-edit-human-rated"]);
                 $payload = $this->test_input($_POST["rocket-edit-payload"]);
                 $height = $this->test_input($_POST["rocket-edit-height"]);
+                $id = $this->test_input($_POST["rocket-edit-id"]);
 
-                $rocket = new Rocket($_POST['rocket-edit-id'], $manufacturer_id, $this->imageName, $name, $human_rated, $payload, $height);
+                $rocket = new Rocket($id, $manufacturer_id, $this->imageName, $name, $human_rated, $payload, $height);
                 //$rocket = new Rocket($_POST['rocket-edit-id'], manufacturer_id: $_POST['rocket-edit-manufacturer'], name: $_POST['rocket-edit-name'], human_rated: $_POST['rocket-edit-human-rated'], payload: $_POST['rocket-edit-payload'], height: $_POST['rocket-edit-height']);
-                $this->storage->updateRocket($_POST['rocket-edit-id'], $rocket);
+                $this->storage->updateRocket($id, $rocket);
             }
         }
     }
@@ -71,12 +72,17 @@ class App
     private function validateFormEdit(): bool
     {
         $name = $_POST ["rocket-edit-name"];
-        if (empty($_POST["rocket-edit-name"]) || !preg_match ("/^\w+( \w+)*$/", $name) || strlen ($name) > 20) {
+        if (empty($_POST["rocket-edit-name"]) || !preg_match ("/^\w+( \w+)*$/", $name) || strlen($name) > 20) {
             return false;
         }
 
         $humanRated = $_POST['rocket-edit-human-rated'];
         if (!preg_match ("/^[01]*$/", $humanRated)) {
+            return false;
+        }
+
+        $id = $_POST['rocket-edit-id'];
+        if (!preg_match ("/^[0-9]*$/", $id)) {
             return false;
         }
 
