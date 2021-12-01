9180401ba3c5ec87972a9e0a6b5e2a35d3274d0f
Filipson <filip.sudora@gmail.com>
2021-12-01 09:38:53 +0100

    Image validation 2

--- classes/App.php
+++ classes/App.php
@@ -151,33 +151,16 @@ class App
             $path = "../assets/rockets/$this->imageName";
             move_uploaded_file($_FILES['file']['tmp_name'], $path);
         } else {
-            switch ($_FILES['file']['error']) {
-                case UPLOAD_ERR_INI_SIZE:
-                    $message = "The uploaded file exceeds the upload_max_filesize directive in php.ini";
-                    break;
-                case UPLOAD_ERR_FORM_SIZE:
-                    $message = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
-                    break;
-                case UPLOAD_ERR_PARTIAL:
-                    $message = "The uploaded file was only partially uploaded";
-                    break;
-                case UPLOAD_ERR_NO_FILE:
-                    $message = "No file was uploaded";
-                    break;
-                case UPLOAD_ERR_NO_TMP_DIR:
-                    $message = "Missing a temporary folder";
-                    break;
-                case UPLOAD_ERR_CANT_WRITE:
-                    $message = "Failed to write file to disk";
-                    break;
-                case UPLOAD_ERR_EXTENSION:
-                    $message = "File upload stopped by extension";
-                    break;
-
-                default:
-                    $message = "Unknown upload error";
-                    break;
-            }
+            $message = match ($_FILES['file']['error']) {
+                UPLOAD_ERR_INI_SIZE => "The uploaded file exceeds the upload_max_filesize directive in php.ini",
+                UPLOAD_ERR_FORM_SIZE => "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form",
+                UPLOAD_ERR_PARTIAL => "The uploaded file was only partially uploaded",
+                UPLOAD_ERR_NO_FILE => "No file was uploaded",
+                UPLOAD_ERR_NO_TMP_DIR => "Missing a temporary folder",
+                UPLOAD_ERR_CANT_WRITE => "Failed to write file to disk",
+                UPLOAD_ERR_EXTENSION => "File upload stopped by extension",
+                default => "Unknown upload error",
+            };
             die($message);
         }
     }
